6d09e188d04fb4e0e9b70bd0e8ba07e3f5b3cd06
albertter <alberdder@gmail.com>
2020-02-15 21:34:33 +0800

    update VGG.py

---
+++ models/VGG.py
@@ -0,0 +1,99 @@
+import torch.nn as nn
+import torch
+import math
+
+
+class VGG(nn.Module):
+    def __init__(self, features, num_classes = 10):
+        super(VGG, self).__init__()
+        self.features = features
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(512, 512),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(512, 512),
+            nn.ReLU(True),
+            nn.Linear(512, num_classes),
+        )
+        # Initialize weights
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode = 'fan_out', nonlinearity = 'relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        x = self.features(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
+
+
+def make_layers(cfg, batch_norm = False):
+    layers = []
+    in_channels = 3
+    for v in cfg:
+        if v == 'M':
+            layers += [nn.MaxPool2d(kernel_size = 2, stride = 2)]
+        else:
+            conv2d = nn.Conv2d(in_channels, v, kernel_size = 3, padding = 1)
+            if batch_norm:
+                layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace = True)]
+            else:
+                layers += [conv2d, nn.ReLU(inplace = True)]
+            in_channels = v
+    return nn.Sequential(*layers)
+
+
+cfgs = {
+    'A': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'B': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'D': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'E': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+}
+
+
+def vgg11():
+    return VGG(make_layers(cfgs['A']))
+
+
+def vgg11_bn():
+    return VGG(make_layers(cfgs['A'], True))
+
+
+def vgg13():
+    return VGG(make_layers(cfgs['B']))
+
+
+def vgg13_bn():
+    return VGG(make_layers(cfgs['B'], True))
+
+
+def vgg16():
+    return VGG(make_layers(cfgs['D']))
+
+
+def vgg16_bn():
+    return VGG(make_layers(cfgs['D'], True))
+
+
+def vgg19():
+    return VGG(make_layers(cfgs['E']))
+
+
+def vgg19_bn():
+    return VGG(make_layers(cfgs['E'], True))
+
+
+model = vgg19_bn()
+x = torch.randn(1, 3, 32, 32)
+y = model(x)
+print(model)
+print(y.shape)
